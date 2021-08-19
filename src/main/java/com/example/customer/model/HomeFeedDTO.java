075422af007726e278a0d36433e1c850ffddc3f8
anas1606 <darshan@eliteevince.in>
2021-08-19 16:13:57 +0530

    update

--- src/main/java/com/example/customer/model/HomeFeedDTO.java
+++ src/main/java/com/example/customer/model/HomeFeedDTO.java
@@ -9,11 +9,13 @@ public class HomeFeedDTO {
     private String productName;
     private String desc;
     private String price;
+    private String category;
 
     public HomeFeedDTO(Product product) {
         this.productid = product.getId();
         this.productName = product.getName();
         this.desc = product.getDescription();
         this.price = product.getPrice().toString();
+        this.category = product.getCategory().getName();
     }
 }
